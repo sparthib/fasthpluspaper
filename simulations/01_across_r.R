4d3f6b829e04db80a109b00bac152e4cbebf91b0
Sowmya Parthiban <sparthi1@jh.edu>
2022-04-15 09:32:38 -0400

    used purrr for across k and r, created plots

--- simulations/01_across_r.R
+++ simulations/01_across_r.R
@@ -3,6 +3,9 @@
 # install_github(repo="ntdyjack/fasthplus", ref = "main")#load relevant packages
 library(fasthplus)
 library(here)
+library(usethis)
+library(purrr)
+library(ggplot2)
 
 
 ## output directories
@@ -77,56 +80,75 @@ simulate_data <- function(balance, spread, n_cells = 1000, n_genes = 500,
  
 }
 
-data <- simulate_data(balance = 'balanced', spread = 'easy', k = 3)
+easy_balanced_data <- simulate_data(balance = 'balanced', spread = 'easy', k = 3)
+hard_balanced_data <- simulate_data(balance = 'balanced', spread = 'hard', k = 3)
+easy_imbalanced_data <- simulate_data(balance = 'imbalanced', spread = 'easy', k = 3)
+hard_imbalanced_data <- simulate_data(balance = 'imbalanced', spread = 'hard', k =3)
 
+par(mar=c(1, 1, 1, 1))
+#check true data 
+# plot(easy_balanced_data$true_data[,1], easy_balanced_data$true_data[,2])
+# plot(hard_imbalanced_data$true_data[,1], hard_imbalanced_data$true_data[,2])
 
 # plot_fhp is a function that takes simulated data as input
 # performs kmeans for a set of ks 
 # calculates hpb for all ks for a fixed t and r 
 # and plots results 
 
-plot_fhp <- function(data, k, t= 10, r = 10){ 
-  ks<- seq(k)
-  fhp_res<- c()
-  res <- kmeans(data$obs_data, centers = k)
-  for(k in ks){
-    fhp_res<- c(fhp_res, fasthplus::hpb(D = data$obs_data, L= res$cluster, t= t,r=t))
-  }
+ks<- seq(4)
+kmeans_for_ks <- ks |> map(function(x) kmeans(easy_balanced_data$obs_data, centers =x))
   
-  plot(ks, fhp_res)
-
-}
-
-plot_fhp(data = data, k = 10, t = 10, r = 10)
-
-data <- simulate_data(balance = 'balanced', spread = 'easy', k = 3)
-
-ks<- seq(10)
-fhp_res<- c()
-t = 10
-r = 10
-
-k =3 
-
-lapply(ks, kmeans(data$obs_data, centers = k))
-
-for(k in ks){
-  kmeans_res <- kmeans(data$obs_data, centers = k)
-  fhp_res<- append(fhp_res, fasthplus::hpb(D = data$obs_data, L= kmeans_res$cluster, t= t,r=t))
-}
+fhp_res <- kmeans_for_ks |> map(function(x) fasthplus::hpb(D = easy_balanced_data$obs_data, 
+                                          L = x$cluster, t = 10, r = 10)) 
 
 plot(ks, fhp_res)
 
 
+plot_fhp_across_k <- function(data, k, t, r){
+  ks<- seq(k)
+  kmeans_for_ks <- ks |> map(function(x) kmeans(data$obs_data, centers =x))
+  
+  fhp_res <- kmeans_for_ks |> map(function(x) fasthplus::hpb(D = data$obs_data, 
+                                                             L = x$cluster, t = t, r = r)) 
+  #plot(ks, fhp_res)
+  results <- do.call(rbind, Map(data.frame, ks=ks, fhp_res=fhp_res))
+  
+  ggplot( data = results, aes(x=ks, y=fhp_res))+
+    geom_line()+
+    geom_point()
 
-
-##easy data, imbalanced 
-
+}
 
 ##plot x -axis different k, y is a single r 
+plot_fhp_across_k(easy_balanced_data, k = 6, t = 10, r = 10)
+plot_fhp_across_k(easy_imbalanced_data, k = 6, t = 10, r = 10)
+plot_fhp_across_k(hard_balanced_data, k = 6, t = 10, r = 10)
+plot_fhp_across_k(hard_imbalanced_data, k = 6, t = 10, r = 10)
+
+
+
 ##plot x- axis different rs, y compare to true H+ 
-##easy medium and hard data in terms of means and sds 
-##how would balance affect? 
+plot_fhp_across_r<- function(data, k, t, r){
+  rs<- seq(r)
+  k_means <- kmeans(data$obs_data, centers =k)
+  
+  fhp_res <- rs |> map(function(x) fasthplus::hpb(D = data$obs_data, 
+                                                             L = k_means$cluster,
+                                                  t = t, r = x)) 
+  
+  results <- do.call(rbind, Map(data.frame, rs=rs, fhp_res=fhp_res))
+  
+  ggplot( data = results, aes(x=rs, y=fhp_res))+
+    geom_line()+
+    geom_point()
+  
+}
+
+plot_fhp_across_k(easy_balanced_data, k = 6, t = 10, r = 10)
+plot_fhp_across_k(easy_imbalanced_data, k = 6, t = 10, r = 10)
+plot_fhp_across_k(hard_balanced_data, k = 6, t = 10, r = 10)
+plot_fhp_across_k(hard_imbalanced_data, k = 6, t = 10, r = 10)
+
 
 
 
@@ -134,45 +156,5 @@ plot(ks, fhp_res)
 
 
 
-#take your obs data (500 genes) apply to k means. 
-#what happens when r varies, does hpb always get it right 
-
-
 
 #Explore what happens when you apply hpb()  varying the number of bootstrap samples (`r).
-ptm <- proc.time()
-fasthplus::hpb(D = data$obs_data, L= data$true_cluster_id, t=10,r=10)
-#D = truedata or obsdata? 
-proc.time() - ptm
-
-# user  system elapsed 
-# 0.003   0.002   0.006 
-x_mus = c(1 ,5, 10) 
-y_mus = c(1,5,10) 
-n_cells = 1000
-n_genes = 3
-x_sds = c(0.1,0.1,0.1)
-y_sds = c(0.1,0.1,0.1)
-comp1 <- sample(seq_len(3), prob= c(0.1, 0.1, 0.8), size=n_cells, replace=TRUE)
-samples_1 <- cbind(rnorm(n= n_cells, mean=x_mus[comp1],sd=x_sds[comp1]),
-                   rnorm(n=n_cells, mean=y_mus[comp1],sd=y_sds[comp1])) #simulate a bivariate distribution
-
-plot(samples_1[,1], samples_1[,2])
-
-#project 2D to higher dim space 
-proj <- matrix(rnorm(n_genes*n_cells), nrow=n_genes, ncol=2) 
-A1 <- samples_1 %*% t(proj) 
-dim(A1)
-
-# Add normally distributed noise - true info is only captured by the two features, 
-#projecting into a higher dim space means embedding true info with some noise in higher dim. 
-
-A1 <- A1 + rnorm(n_genes*n_cells)
-rownames(A1) <- paste0("Cell", seq_len(n_cells), "-1")
-colnames(A1) <- paste0("Gene", seq_len(n_genes))
-
-
-list("true_center" = cbind("x" = x_mus, "y" = y_mus)
-     "true_cluster_id" = comp1,
-     "true_data" = samples_1, 
-     "obs_data" = A1)
